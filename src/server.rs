42c0d8abb8391cb16d137733056ae55a083cc90d
Keshav Kumar <56392832+krkeshav@users.noreply.github.com>
2023-06-18 16:36:58 +0000

    Added http listener which logs incoming requests

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
-
+use std::net::TcpListener;
+use std::io::Read;
 // Definition of server structure
 pub struct Server {
     addr: String
@@ -14,7 +15,24 @@ impl Server {
 
     // run method starts the http server and keeps looping forever
     pub fn run(self) {
-        println!("Listening on {}", self.addr)
+        println!("Listening on {}", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer))
+                        },
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                },
+                Err(e) => println!("Failed to establish connection: {}", e),
+                _ => {} // This acts like default condition and hence catches all
+            }
+        }
     }
 }
 
